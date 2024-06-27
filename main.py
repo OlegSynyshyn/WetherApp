ddfc64e22101f02e424d059de40f41c7588badb2
Oleh <oleg.synyshyn10@gmail.com>
2024-06-27 17:54:33 +0300

    123

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDRectangleFlatIconButton
+
+class HomeScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        return MDLabel(text="Hello, World", halign="center")
+        Builder.load_file("style.kv")
+        self.screen = HomeScreen(name="home")
+        return self.screen
 
 
 MainApp().run()
